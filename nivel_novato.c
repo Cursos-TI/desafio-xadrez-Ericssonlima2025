a9f1a9086694ef8daa6e1794ad15965cea20bd74
Ericssonlima2025 <ericssonvirtuall@hotmail.com>
2025-06-05 14:56:06 +0000

    nivel novato

---
+++ nivel_novato.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+
+    // Movimento da Torre (FOR)
+    // A Torre se move em linha reta, neste caso, 5 casas para a direita.
+    printf("Movimento da Torre:\n");
+    for (int i = 0; i < 5; i++) {
+        printf("Direita\n");
+    }
+
+    // Movimento do Bispo (WHILE)
+    // O Bispo se move na diagonal. Vamos simular 5 casas para cima e à direita.
+    printf("\nMovimento do Bispo:\n");
+    int j = 0;
+    while (j < 5) {
+        printf("Cima, Direita\n");
+        j++;
+    }
+
+    // Movimento da Rainha (DO-WHILE)
+    // A Rainha se move em todas as direções. Vamos simular 8 casas para a esquerda.
+    printf("\nMovimento da Rainha:\n");
+    int k = 0;
+    do {
+        printf("Esquerda\n");
+        k++;
+    } while (k < 8);
+
+    return 0;
+}
